3365d2828962fcf2a171e999b4f6293fed1789ec
harrys1000rr <harry@DESKTOP-NC80NV3.localdomain>
2022-11-02 10:00:31 +0000

    task10

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,8 @@ import { React, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getTopics } from "../api";
 
+export const Nav = ({ setSortBy, setOrder }) => {
 
-const Nav = () => {
   const [topicList, setTopicList] = useState([]);
   useEffect(() => {
     getTopics().then(({ data: { topics } }) => {
@@ -11,16 +11,21 @@ const Nav = () => {
     });
   }, []);
 
+  const handleSortChange = (event) => {
+    setSortBy(event.target.value);
+  };
+
+  const handleOrderChange = (event) => {
+    setOrder(event.target.value);
+  };
+
     return (
     <section>
       <section>
       </section>
       <section >
       <nav>
-          <Link
-            to="/">
-            All
-          </Link>
+        <Link to="/">All</Link>
           {topicList.map((topic) => {
             return ( 
             <p  key={topic.slug}><Link
@@ -28,13 +33,27 @@ const Nav = () => {
     {topic.slug}          
     
               </Link>
-              </p>
-            );
-          })}
+              </p>)})}
         </nav>
+        <section className="filter-order">
+          <select className="sort-by-filter" onChange={handleSortChange}>
+            <option>
+              Sort by...
+            </option>
+            <option value="created_at">Created at</option>
+            <option value="votes">Votes</option>
+            <option value="title">Title</option>
+          </select>
+          <select className="order-by-filter" onChange={handleOrderChange}>
+            <option>
+              Order by
+            </option>
+            <option value="DESC">Desc</option>
+            <option value="ASC">Asc</option>
+          </select>
         </section>
+      
       </section>
+    </section>
   );
 };
-
-export default Nav
